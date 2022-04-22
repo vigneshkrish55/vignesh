883bd57a0a9570b562ade288ee8afd2ed429fb44
vigneshkrish55 <vigneshkrish55@gmail.com>
2022-04-22 09:26:19 +0530

    first commit

--- vignesh/src/vignesh/hello.java
+++ vignesh/src/vignesh/hello.java
@@ -1,10 +1,10 @@
 package vignesh;
 
-public class Helloworld {
+public class hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-     System.out.println("hello world");
+
 	}
 
 }
